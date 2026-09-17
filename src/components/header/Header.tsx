4b11316eb36799dbabc93948ac063b9132642bd4
Facundo D'addese <facundo.daddese19@gmail.com>
2026-09-16 22:40:23 -0300

    creo el contexto y el reducer

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="text-center p-7">
+      <h1 className="text-[40px]">Planificador de gastos</h1>
+    </header>
+  );
+};
+
+export default Header;
